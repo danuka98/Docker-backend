0e873feeb196f7827b33b3e16587708ee8041593
thara99 <it19148014@my.sliit.lk>
2022-05-16 14:08:29 +0530

    fullBackEnd

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ app.use(
 //Import Routes
 const product = require("./routes/Product.Routes");
 
+//Tharaka(import routes)
+const empoyee = require('./routes/Employee.Routes');
+const leaves = require('./routes/Leaves.Router');
+
 //use Routes
 app.use("/api/v1/product", product);
+
+//tharaka(use routes)
+app.use('/api/v1/employee',empoyee)
+app.use('/api/v1/leaves',leaves)
 
 module.exports = app;
